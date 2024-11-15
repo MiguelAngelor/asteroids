9491efa1d3206c8e7bd616c0e51eab533fd394c6
MiguelAngelor <miguel.orellana.morales@gmail.com>
2024-11-14 21:20:51 -0500

    B. Added Player and Circle/Triangle class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import * #Import game constants
+from player import *
 
 def main():
 #Opening info:
@@ -10,14 +11,33 @@ def main():
 #Setting up the Screen Variable
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
+#Time clock object:
+    Clock = pygame.time.Clock()
+    dt = 0
+
+#Spawn Player
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x,y)
+
 #Infinite Loop for the Game:
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        #Screen Draw
         screen.fill((0,0,0))
+
+        #Player refresh
+        player.draw(screen)
+
+        #flip After all drawing is done
         pygame.display.flip()
+
+        #Making it 60 fps
+        dt = Clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
